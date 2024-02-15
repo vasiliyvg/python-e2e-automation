609f3dbd745b5c7b636d4885596b2991272124b6
Vasyl Bykadorov <vbykadorov@iodinesoftware.com>
2024-02-15 11:59:17 +0100

    added tasks and solutions

--- src/factorial.py
+++ src/factorial.py
@@ -6,12 +6,7 @@ def factorial(num):
     if num == 0:
         return 1
 
-    i = 0
-    f = 1
-    while i < num:
-        i += 1
-        f *= i
-    return f
+    return num * factorial(num - 1)
 
 num = 5
 print(factorial(num))
